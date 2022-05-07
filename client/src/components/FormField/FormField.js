dd05455475a61e9b03320104fa9f342d1abe5ff3
koritsa99 <graduses9@gmail.com>
2022-05-07 12:48:10 +0300

    Created register form

--- client/src/components/FormField/FormField.js
+++ client/src/components/FormField/FormField.js
@@ -1,9 +1,11 @@
 import styles from './FormField.module.css';
 
-function FormField({ label, children }) {
+function FormField({ label, htmlFor, children }) {
   return (
     <div className={styles.field}>
-      <label className={styles.label}>{label}</label>
+      <label className={styles.label} htmlFor={htmlFor}>
+        {label}
+      </label>
       <div className={styles.inputBox}>{children}</div>
     </div>
   );
--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,6 +1,10 @@
 import { useDispatch } from 'react-redux';
+import { useFormik } from 'formik';
 
 import { register } from '../../redux/auth/auth.slice';
+import FormField from '../../components/FormField';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const data = {
   username: 'dev1',
@@ -10,14 +14,77 @@ const data = {
 
 function Register() {
   const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+      password: '',
+      repeatPassword: '',
+    },
+    validateOnBlur: true,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
 
   return (
-    <div>
+    <form onSubmit={formik.handleSubmit}>
       <h1>Register</h1>
-      <button type="button" onClick={() => dispatch(register(data))}>
+      <FormField label="Username" htmlFor="username">
+        <Input
+          type="text"
+          autoComplete="nickname"
+          placeholder="Username"
+          name="username"
+          id="username"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          required
+        />
+      </FormField>
+      <FormField label="Password" htmlFor="password">
+        <Input
+          type="password"
+          autoComplete="new-password"
+          placeholder="Password"
+          name="password"
+          id="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          required
+        />
+      </FormField>
+      <FormField label="Repeat password" htmlFor="repeatPassword">
+        <Input
+          type="password"
+          autoComplete="new-password"
+          placeholder="Repeat password"
+          name="repeatPassword"
+          id="repeatPassword"
+          value={formik.values.repeatPassword}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          required
+        />
+      </FormField>
+      <FormField label="Email" htmlFor="email">
+        <Input
+          type="email"
+          placeholder="Email"
+          name="email"
+          id="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          required
+        />
+      </FormField>
+      <Button type="submit" variant="secondary">
         Register
-      </button>
-    </div>
+      </Button>
+    </form>
   );
 }
 
