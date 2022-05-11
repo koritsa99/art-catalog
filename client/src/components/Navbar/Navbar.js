101d1681768b4479366594d0ca6859c313d7ab05
koritsa99 <graduses9@gmail.com>
2022-05-11 17:34:47 +0300

    Added userpopup to navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,15 +1,24 @@
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useRef, useState } from 'react';
 
 import styles from './Navbar.module.css';
 import { urls } from '../../config/routes';
 import { getUser } from '../../redux/auth/auth.selectors';
+import { useClickOutside } from '../../hooks/useClickOutside';
 import Container from '../Container';
 import Button from '../Button';
 import Searchbar from '../Searchbar';
 
 function Navbar() {
   const user = useSelector(getUser);
+  const [userPopupOpen, setUserPopupOpen] = useState(false);
+  const popupRef = useRef();
+
+  const closePopup = () => setUserPopupOpen(false);
+  const togglePopup = () => setUserPopupOpen((prev) => !prev);
+
+  useClickOutside(popupRef.current, closePopup);
 
   return (
     <header className={styles.header}>
@@ -39,9 +48,34 @@ function Navbar() {
               </Button>
             )}
             {user && (
-              <Button component={Link} to={`${urls.users}/${user.id}`}>
-                Profile
-              </Button>
+              <div ref={popupRef} className={styles.profile}>
+                <button
+                  type="button"
+                  className={styles.userBtn}
+                  onClick={togglePopup}
+                >
+                  <img
+                    src="/avatar.jpg"
+                    alt="Avatar"
+                    width={50}
+                    height={50}
+                    className={styles.userAvatar}
+                  />
+                </button>
+                {userPopupOpen && (
+                  <div className={styles.userPopup} onClick={closePopup}>
+                    <Button component={Link} to={`${urls.users}/${user.id}`}>
+                      Profile
+                    </Button>
+                    <Button component={Link} to={`${urls.users}/${user.id}`}>
+                      Settings
+                    </Button>
+                    <Button component={Link} to={`${urls.users}/${user.id}`}>
+                      Logout
+                    </Button>
+                  </div>
+                )}
+              </div>
             )}
           </div>
         </div>
